9741459c5b83fe3a68964977d0add20c20bc5b1b
ankitdcx <328232957+ankitdcx@users.noreply.github.com>
2026-09-13 10:16:44 +0530

    WP-007 Step 2: export DesignEpoch binding types

--- implementation/garden_kernel/__init__.py
+++ implementation/garden_kernel/__init__.py
@@ -7,6 +7,11 @@ from .evolution_actions import (
     CONTRACTS, Condition, EvolutionAction, EvolutionActionContract,
     EvolutionVerb, contract_for, validate_action_shape,
 )
+from .evolution_epoch import (
+    BindingStatus, BindingValidation, EvolutionArtifactBinding,
+    EvolutionArtifactKind, require_current_for_accumulation,
+    validate_evolution_binding,
+)
 from .extractor import ClosureManifest, GapRecord, ObligationExtractor
 from .store import KnowledgeStore
 from .verifier import VerifierClass, VerifierRegistry, VerificationReceipt
@@ -16,6 +21,9 @@ __all__ = [
     "Obligation", "ReceiptStatus", "TypedValue", "parse_claim",
     "CONTRACTS", "Condition", "EvolutionAction", "EvolutionActionContract",
     "EvolutionVerb", "contract_for", "validate_action_shape",
+    "BindingStatus", "BindingValidation", "EvolutionArtifactBinding",
+    "EvolutionArtifactKind", "require_current_for_accumulation",
+    "validate_evolution_binding",
     "ClosureManifest", "GapRecord", "ObligationExtractor", "KnowledgeStore",
     "VerifierClass", "VerifierRegistry", "VerificationReceipt",
 ]
